ce13af20abbf6e23d80551f03b7862c286ada668
Denner Marchi <denner883@hotmail.com>
2020-05-16 21:12:56 -0300

    Trocando o nome do caminho de 'home' para 'index'

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', authRouter);
-app.use('/home', indexRouter);
+app.use('/index', indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
