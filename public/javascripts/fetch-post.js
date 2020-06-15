1a5740bd131473e19ba34eeabf336019f9a4c073
Denner Marchi <denner883@hotmail.com>
2020-06-15 05:08:12 -0300

    Adicionando comentário com mais detalhes no fetch-post

--- public/javascripts/fetch-post.js
+++ public/javascripts/fetch-post.js
@@ -2,6 +2,8 @@ async function solicitarPost(url, infoBody) {
     if (!url) {
         throw new TypeError('Url não pode estar vazio');
     }
+    
+    /* as informações do body deve ser uma string (use JSON.stringfy antes de enviar)*/
     if (!infoBody) {
         throw new TypeError('Body não pode ser vazio!');
     }
