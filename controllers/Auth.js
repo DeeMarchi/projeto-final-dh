77dd6a7596bfd38569f405d6dfc0bd833cde5dac
Denner Marchi <denner883@hotmail.com>
2020-06-18 14:01:41 -0300

    Quick-fix tamanho avatar usuário e cadastro

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -152,32 +152,31 @@ const AuthController = {
                 titulo: 'Cadastrar | Erro', 
                 erros: listaErros.errors ,
             });
-        } else {
-            const { 
-                nome, 
-                apelido, 
-                email, 
-                senha 
-            } = req.body;
-            const senhaCripto = bcrypt.hashSync(senha, 10);
-
-            const novoUsuario = await Usuario.create({
-                nome: nome,
-                apelido: apelido,
-                email: email,
-                senha: senhaCripto,
-            });
-            req.session.usuario = {
-                id: novoUsuario.id,
-                nome: novoUsuario.nome,
-                email: novoUsuario.email,
-                apelido: novoUsuario.apelido,
-                imagem_url: usuario.imagem_url,
-                resumo: usuario.resumo,
-            }
-
-            return res.redirect('/index');
         }
+        const { 
+            nome, 
+            apelido, 
+            email, 
+            senha 
+        } = req.body;
+        const senhaCripto = bcrypt.hashSync(senha, 10);
+
+        const novoUsuario = await Usuario.create({
+            nome: nome,
+            apelido: apelido,
+            email: email,
+            senha: senhaCripto,
+        });
+        req.session.usuario = {
+            id: novoUsuario.id,
+            nome: novoUsuario.nome,
+            email: novoUsuario.email,
+            apelido: novoUsuario.apelido,
+            imagem_url: novoUsuario.imagem_url,
+            resumo: novoUsuario.resumo,
+        }
+
+        return res.redirect('/index');
     },
 };
 
