4c2bb227472a7f837afe9ed9ca536ef67a061492
Denner Marchi <denner883@hotmail.com>
2020-06-20 12:38:15 -0300

    Adicionando dotenv ao projeto

--- config/database.js
+++ config/database.js
@@ -1,7 +1,19 @@
+require('dotenv').config();
+
+const {
+    DB_USER,
+    DB_PASS,
+    DB_DATABASE,
+    DB_HOST,
+    DB_PORT,
+    DB_DIALECT,
+} = process.env;
+
 module.exports = {
-    username: "root",
-    password: null,
-    database: "trip-diary",
-    host: "localhost",
-    dialect: "mysql",
+    username: DB_USER,
+    password: DB_PASS,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: DB_DIALECT,
 };
