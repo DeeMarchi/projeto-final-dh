75d63139e1b1446487efe8ad630a64e0b2dc1bd9
otocampos <90807@pmerj.rj.gov.br>
2020-06-23 04:21:59 -0300

    curtir e descurtir comentario

--- controllers/Comentario.js
+++ controllers/Comentario.js
@@ -11,34 +11,66 @@ const {
 const moment = require('moment')
 const comentarioController = {
 
-  
+
     criarComentario: async (req, res) => {
-        let { comentario } = req.body
+        let {
+            comentario
+        } = req.body
         let idRoteiro = req.params.id
 
-       let dado = await Comentario.create({ conteudo: comentario, usuario_id: req.session.usuario.id, roteiro_id: idRoteiro })
-res.redirect('back')
+        let dado = await Comentario.create({
+            conteudo: comentario,
+            usuario_id: req.session.usuario.id,
+            roteiro_id: idRoteiro
+        })
+        res.redirect('back')
     },
     curtirComentarioRoteiro: async (req, res) => {
 
-      idComentario = req.params.id
+        idComentario = req.params.id
 
-        
-        
-        let comentario = await Comentario.findByPk(idComentario)
-        
-        comentario.likes = ++comentario.likes;
-        await comentario.save();
+        let curtidaComentario = await CurtidaComentario.findOne({
+            where: {
+                usuario_id: req.session.usuario.id,
+                comentario_id: idComentario
+            }
+        })
 
-        await CurtidaComentario.create({comentario_id:idComentario,usuario_id:req.session.usuario.id})
+        if (curtidaComentario == undefined) {
+            let comentario = await Comentario.findByPk(idComentario)
 
-        res.redirect('back')
+            comentario.likes = ++comentario.likes;
+            await comentario.save();
+
+            await CurtidaComentario.create({
+                comentario_id: idComentario,
+                usuario_id: req.session.usuario.id
+            })
+            res.status(201).send(`Like${idComentario}`)
+
+
+        } else {
+            let comentario = await Comentario.findByPk(idComentario)
+            comentario.likes = --comentario.likes;
+            await comentario.save();
+
+            await CurtidaComentario.destroy({
+                where: {
+                    usuario_id: req.session.usuario.id,
+                    comentario_id: idComentario
+                }
+            });
+            res.status(200).send(`Deslike${idComentario}`);
+        }
+
+
+        //res.redirect('back')
 
 
 
     },
 
-    
+
 
 };
 
