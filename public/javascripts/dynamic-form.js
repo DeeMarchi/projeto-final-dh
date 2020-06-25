56628e0445179ad2e5602e1fc550ae92b11976ad
Denner Marchi <denner883@hotmail.com>
2020-06-25 01:47:15 -0300

    colocando required nos campos criar roteiro e mais correção no ABBR do 'ver-roteiro'

--- public/javascripts/dynamic-form.js
+++ public/javascripts/dynamic-form.js
@@ -1,78 +1,62 @@
- var dia = 1;
+var dia = 1;
 
- function roteiroCampo() {
-     let opcoes = document.getElementsByClassName('option-coin')
+function roteiroCampo() {
+    let opcoes = document.getElementsByClassName('option-coin')
 
+    if (dia < 30) {
+        dia++;
+    }
+    if (dia >= 30) {
+        document.getElementById('adicionarDia').style.visibility = 'hidden';
+    }
+    var objTo = document.getElementById('roteiro-campos');
+    var divtest = document.createElement('div');
 
-     if (dia < 30) {
-         dia++;
-     }
-     if (dia >= 30) {
-         document.getElementById('adicionarDia').style.visibility = 'hidden';
-     }
-     var objTo = document.getElementById('roteiro-campos')
-     var divtest = document.createElement('div');
-     //	divtest.setAttribute("class", "form-group removeclass"+room);
-     //var rdiv = 'removeclass'+room;
+    document.getElementById('btnRemover').style.visibility = 'visible';
+    if (dia <= 30) {
+        document.getElementById('qntDias').innerHTML = contadorDias();
+        document.getElementById("dias").value = dia;
+    }
+    if (dia <= 30) {
+        //' + dia + '
+        let moedas = [];
 
-     document.getElementById('btnRemover').style.visibility = 'visible';
-     if (dia <= 30) {
-         document.getElementById('qntDias').innerHTML = contadorDias();
-         document.getElementById("dias").value = dia;
+        for (let i = 0; i < opcoes.length; i++) {
 
+            moedas.push(opcoes[i].outerHTML);
 
-     }
-     if (dia <= 30) {
-         //' + dia + '
-         let moedas = []
+        }
 
-         for (let i = 0; i < opcoes.length; i++) {
+        divtest.innerHTML = `<div id="principal" class="shadow mt-2 form-group dynamic-element list-group list-group-item flex-col align-items-start rounded-lg"> <div class="row"> <div class="col-1 "> <div class="col-1 mb-2 mt-n2 "><h3><span class=" shadow badge badge-primary float-left">Dia ${dia}</span></h3></div> </div> </div> <div class="form-group row"> <div class=" col-sm-4 col-lg-6 col-md-5 float-left"> <label><strong>Relatos do dia:</strong></label> <textarea type="text" name="relato${dia}" class="form-control" row="5" placeholder="Descreva como foram os passeios realizados neste dia" required></textarea> <sub class="form-text text-muted"></s> </div> <div class="col-sm-4 col-lg-6 col-md-6 float-left"> <label><strong>Locais por onde passei:</strong></label> <input type="text" data-role="taginput" data-cls-tag-title="text-bold fg-white" data-cls-tag="bg-olive" data-cls-tag-remover="bg-darkOlive fg-white" data-tag-trigger="Comma" data-random-color="true" data-max-tags="5" name="locais${dia}" placeholder="Locais que visitou neste dia" required> <sub class="form-text text-muted">Locais que visitou neste dia</s> </div> </div> <div class="form-group row"> <div class="col-sm-4 col-lg-6 col-md-6 float-left"> <label class="col-form-label"><strong>Média de Gastos:</strong></label> <div class="input-group mb-3"> <div class="input-group-prepend"> <select class="form-control custom select" name="selectMoeda${dia}">${moedas}</select> </div> <input type="number" name="valor${dia}" class="form-control" aria-label="Amount (to the nearest dollar)" required> <div class="input-group-append"> <span class="input-group-text">.00</span> </div> </div> </div> </div> </div>`;
 
-             moedas.push(opcoes[i].outerHTML);
+        divtest.setAttribute('class', 'form-group removeclass' + dia);
 
-         }
+        objTo.appendChild(divtest);
+        document.getElementsByClassName('removeclass2').scrollIntoView();
+    }
+}
 
-         /*     moedaViagem.forEach(element => {
-    moedas.push(`<option value=${element.id}>${element.simbolo}</option>`)
+function removerCampoRoteiro() {
+    if (dia == 2) {
+        document.getElementById('btnRemover').style.visibility = 'hidden';
+    }
 
-});  */
+    $('.removeclass' + dia).remove();
+    dia = dia - 1;
+    document.getElementById('qntDias').innerHTML = contadorDias();
+    document.getElementById("dias").value = dia;
 
-         console.log(moedas)
+    if (dia < 30) {
+        document.getElementById('adicionarDia').style.visibility = 'visible';
+    }
+}
 
+const contadorDias = () => {
+    return 'Quantidade total de dias: ' + dia;
+}
 
-         divtest.innerHTML = `<div id="principal" class="shadow mt-2 form-group dynamic-element list-group list-group-item flex-col align-items-start rounded-lg"> <div class="row"> <div class="col-1 "> <div class="col-1 mb-2 mt-n2 "><h3><span class=" shadow badge badge-primary float-left">Dia ${dia}</span></h3></div> </div> </div> <div class="form-group row"> <div class=" col-sm-4 col-lg-6 col-md-5 float-left"> <label><strong>Relatos do dia:</strong></label> <textarea type="text" name="relato${dia}" class="form-control" row="5" placeholder="Descreva como foram os passeios realizados neste dia"></textarea> <sub class="form-text text-muted"></s> </div> <div class="col-sm-4 col-lg-6 col-md-6 float-left"> <label><strong>Locais por onde passei:</strong></label> <input type="text" data-role="taginput" data-cls-tag-title="text-bold fg-white" data-cls-tag="bg-olive" data-cls-tag-remover="bg-darkOlive fg-white" data-tag-trigger="Comma" data-random-color="true" data-max-tags="5" name="locais${dia}" placeholder="Locais que visitou neste dia"> <sub class="form-text text-muted">Locais que visitou neste dia</s> </div> </div> <div class="form-group row"> <div class="col-sm-4 col-lg-6 col-md-6 float-left"> <label class="col-form-label"><strong>Média de Gastos:</strong></label> <div class="input-group mb-3"> <div class="input-group-prepend"> <select class="form-control custom select" name="selectMoeda${dia}">${moedas}</select> </div> <input type="number" name="valor${dia}" class="form-control" aria-label="Amount (to the nearest dollar)"> <div class="input-group-append"> <span class="input-group-text">.00</span> </div> </div> </div> </div> </div>`;
+document.getElementById('qntDias').innerHTML = contadorDias();
+document.getElementById('dias').setAttribute('value', dia);
 
-         divtest.setAttribute('class', 'form-group removeclass' + dia);
-
-         objTo.appendChild(divtest)
-         document.getElementsByClassName('removeclass2').scrollIntoView();
-     }
- }
-
- function removerCampoRoteiro() {
-     if (dia == 2) {
-         document.getElementById('btnRemover').style.visibility = 'hidden';
-
-     }
-
-     $('.removeclass' + dia).remove();
-     dia = dia - 1;
-     document.getElementById('qntDias').innerHTML = contadorDias();
-     document.getElementById("dias").value = dia;
-
-
-     if (dia < 30) {
-         document.getElementById('adicionarDia').style.visibility = 'visible';
-     }
- }
-
- const contadorDias = () => {
-     return 'Quantidade total de dias: ' + dia;
- }
-
- document.getElementById('qntDias').innerHTML = contadorDias();
- document.getElementById('dias').setAttribute('value', dia);
-
-
- const opcoes = document.getElementsByClassName('option-coin')
- opcoes[0].outerHTML
+const opcoes = document.getElementsByClassName('option-coin')
+opcoes[0].outerHTML
